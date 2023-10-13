e248dd6293fd53039391926c4afbc987f9a9ac0c
Dale McClure <intheazvalley@gmail.com>
2023-10-12 22:05:07 -0400

    fix printjobs

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,19 +119,17 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        if (someJobs.isEmpty()) { // check if empty
+        if(someJobs.isEmpty()) {
             System.out.print("No Results");
         } else {
-            for (HashMap<String, String> job : someJobs) {
+            for(HashMap<String,String> job: someJobs) {
                 System.out.println("\n*****");
-                for (Map.Entry<String, String> aJob : job.entrySet()) { // loop over data
+                for(Map.Entry<String, String> aJob: job.entrySet()) {
                     System.out.println(aJob.getKey() + ": " + aJob.getValue());
                 }
                 System.out.println("*****");
             }
-
-            // System.out.println("printJobs is not implemented yet");
+            //System.out.println("printJobs is not implemented yet");
         }
     }
 }
